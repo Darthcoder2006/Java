8322842876fadd6b2c327e8f06a7cef9c0bdd301
Aadithya S Nair <145871282+Darthcoder2006@users.noreply.github.com>
2025-01-27 08:11:36 +0000

    update 0.4.1

--- labsheet/Lab5.java
+++ labsheet/Lab5.java
@@ -6,4 +6,12 @@ public class Lab5 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         double length, width, height;
-}
+        System.out.println("Enter length: ");
+        length=scanner.nextDouble();
+        System.out.println("Enter width: ");
+        width=scanner.nextDouble();
+        System.out.println("Enter height: ");
+        height=scanner.nextDouble();
+        double volume=length*width*height;
+        System.out.println("Volume: "+volume);
+}}
