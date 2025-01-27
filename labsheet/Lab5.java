2e02b9330fab2bb5ef172e9c6a9637025598ebe5
Aadithya S Nair <145871282+Darthcoder2006@users.noreply.github.com>
2025-01-27 08:20:00 +0000

    Update 0.5

--- labsheet/Lab5.java
+++ labsheet/Lab5.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Lab5 {
     public static void main(String[] args) {
+        System.out.println("A");
         Scanner scanner=new Scanner(System.in);
         double length, width, height;
         System.out.println("Enter length: ");
@@ -14,4 +15,20 @@ public class Lab5 {
         height=scanner.nextDouble();
         double volume=length*width*height;
         System.out.println("Volume: "+volume);
+        System.out.println("B");
+        if (volume>80) {
+            System.out.println("Not Suitable");
+        } else {
+            System.out.println("Suitable");
+        }
+        System.out.println("C");
+        height=height+4;
+        volume=length*width*height;
+        System.out.println("Volume: "+volume);
+        if (volume>80) {
+            System.out.println("New is Not Suitable");
+        } else {
+            System.out.println("New is Suitable");
+        }
+        scanner.close();
 }}
---
+++ labsheet/Lab6.java
@@ -0,0 +1,5 @@
+package labsheet;
+
+public class Lab6 {
+    
+}
